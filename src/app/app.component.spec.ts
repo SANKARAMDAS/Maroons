cf8294eeb36f1165abcc033cc5853e1461faf571
SANKARAM <das95sankaram@gmail.com>
2020-11-10 19:40:38 +0530

    first

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'MaroonsContactUS'`, () => {
+  it(`should have as title 'ContactForm'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('MaroonsContactUS');
+    expect(app.title).toEqual('ContactForm');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to MaroonsContactUS!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ContactForm!');
   });
 });
